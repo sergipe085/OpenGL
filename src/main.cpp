eff4c30bdb16dee1287a6a46f1cc4492356e70a1
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-06-03 23:18:20 -0300

    interpolating colours and EBO (Element Buffer Obj)

--- src/main.cpp
+++ src/main.cpp
@@ -14,35 +14,42 @@
 const GLint WIDTH = 800, HEIGHT = 800;
 const float toRadians = 3.14f / 180.0f;
 
-GLuint VAO, VBO, shader, uniformModel;
-float moveAngle = 0.0f, xPos = 0.0f, yPos = 0.0f;
+GLuint VAO, VBO, IBO, shader, uniformModel, uniformTime;
+float moveAngle = 0.0f, xPos = 0.0f, yPos = 0.0f, time = 0.0f;
 
 //Vertex Shader
-static const char *vShader = "				\
-	#version 330							\n\
-											\n\
-	layout(location = 0) in vec3 pos;		\n\
-											\n\
-	uniform mat4 model;										\n\
-											\n\
-	void main()								\n\
-	{										\n\
-		gl_Position = model * vec4(pos * 0.1, 1.0);			\n\
-	}										\n\
+static const char *vShader = "				                    \
+	#version 330							                    \n\
+											                    \n\
+	layout(location = 0) in vec3 pos;		                    \n\
+											                    \n\
+	uniform mat4 model;										    \n\
+	uniform float time;						                    \n\
+											                    \n\
+	out vec4 vCol;							                    \n\
+											                    \n\
+	void main()								                    \n\
+	{										                    \n\
+		gl_Position = model * vec4(pos, 1.0);			        \n\
+		vCol = vec4(clamp(pos, 0.0, 1.0), 1.0);			        \n\
+	}										                    \n\
 ";
 
 //Fragment Shader
-static const char *fShader = "				\
-	#version 330							\n\
-											\n\
+static const char *fShader = "				                    \
+	#version 330							                    \n\
+											                    \n\
 	in vec4 gl_FragCoord;										\n\
-											\n\
-	out vec4 colour;							\n\
-											\n\
-	void main()								\n\
-	{										\n\
-		colour = vec4(gl_FragCoord.x, gl_FragCoord.y, gl_FragCoord.z, 1.0f);					\n\
-	}										\n\
+	in vec4 vCol;							                    \n\
+											                    \n\
+	out vec4 colour;							                \n\
+											                    \n\
+	uniform float time;						                    \n\
+											                    \n\
+	void main()								                    \n\
+	{										                    \n\
+		colour = vCol;                      					\n\
+	}										                    \n\
 ";
 
 void AddShader(GLuint shaderProgram, const char* shaderCode, GLenum shaderType) {
@@ -100,29 +107,41 @@ void CompileShader() {
     }
 
     uniformModel = glGetUniformLocation(shader, "model");
+    uniformTime  = glGetUniformLocation(shader, "time");
 }
 
 void CreateTriangle() {
+    unsigned int indices[] = {
+        0, 3, 1,
+        1, 3, 2,
+        2, 3, 0,
+        0, 1, 2
+    };
+
     GLfloat vertices[] = {
-        0.0f, 1.0f, 0.0f,
-       -1.0f,-1.0f, 0.0f, 
-        1.0f,-1.0f, 0.0f
+       -1.0f,-1.0f, 0.0f,
+        0.0f,-1.0f, 1.0f,
+        1.0f,-1.0f, 0.0f, 
+        0.0f, 1.0f, 0.0f
     };
 
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
+        glGenBuffers(1, &IBO);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+                glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
         glGenBuffers(1, &VBO);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+                glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-            glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-            glEnableVertexAttribArray(0);
-        
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+            
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 int main() {
@@ -150,6 +169,9 @@ int main() {
         glfwTerminate();
         return 1;
     }
+
+    //Enable Depth Teste
+    glEnable(GL_DEPTH_TEST);
 
     //Get buffer size of window
     int bufferWidth, bufferHeight;
@@ -183,26 +205,34 @@ int main() {
         moveAngle += 0.001f;
         xPos = cos(moveAngle);
         yPos = sin(moveAngle);
+        time = 1;
 
         //Clear Window
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         //Use the shader program
         __glewUseProgram(shader);
 
             glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, glm::vec3(xPos, yPos, 0.0f));
-            model = glm::rotate   (model, moveAngle * 100 * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
-            model = glm::scale    (model, glm::vec3(xPos, yPos, 0.0f));
+            //model = glm::translate(model, glm::vec3(xPos, yPos, 0.0f));
+            model = glm::rotate   (model, moveAngle * 100 * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+            model = glm::scale    (model, glm::vec3(0.2f, 0.2f, 1.0f));
 
             glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+            glUniform1fv(uniformTime, 1, &time);
 
             //Bind VAO (Vertex Array Objects)
             __glewBindVertexArray(VAO);
 
-                //Draw VAO's
-                glDrawArrays(GL_TRIANGLES, 0, 3);
+                //Bind IBO or EBO (Index Buffer Object / Element Buffer Object)
+                glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+
+                    //Draw
+                    glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
+                //Unbind IBO / EBO
+                glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
             //Unbind VAO
             __glewBindVertexArray(0);
